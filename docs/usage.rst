4e973c63ae78ff7d8e237d71dd57a7a44a645a72
Fei Chang <fc1271@nyu.edu>
2021-02-25 16:12:11 +0800

    Initialization

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use lab1_pkg in a project::
+
+    import lab1_pkg
---
+++ tests/test_lab1_pkg.py
@@ -0,0 +1,5 @@
+from lab1_pkg import __version__
+from lab1_pkg import lab1_pkg
+
+def test_version():
+    assert __version__ == '0.1.0'
